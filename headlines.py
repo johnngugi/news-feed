a21f5244cc0fde71682011d40f5842ab8b6de899
johnngugi <johnngugi777@gmail.com>
2016-10-01 14:05:30 +0300

    Add fallback function

--- headlines.py
+++ headlines.py
@@ -1,5 +1,6 @@
+import datetime
 import feedparser
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 import json
 from keys import WEATHER_URL
 import urllib2
@@ -19,15 +20,15 @@ DEFAULTS = {'publication': 'nation',
 
 @app.route('/')
 def home():
-    publication = request.args.get('publication')
-    if not publication:
-        publication = DEFAULTS['publication']
+    publication = get_value_with_fallback('publication')
     articles = get_news(publication)
-    city = request.args.get('city')
-    if not city:
-        city = DEFAULTS['city']
+    city = get_value_with_fallback('city')
     weather = get_weather(city)
-    return render_template('index.html', articles=articles, weather=weather)
+    response = make_response(render_template('index.html', articles=articles, weather=weather))
+    expires = datetime.datetime.now() + datetime.timedelta(days=365)
+    response.set_cookie('publication', publication, expires=expires)
+    response.set_cookie('city', city, expires=expires)
+    return response
 
 
 def get_news(query):
@@ -53,6 +54,14 @@ def get_weather(query):
             'country': parsed['sys']['country']
         }
     return weather
+
+
+def get_value_with_fallback(key):
+    if request.args.get(key):
+        return request.args.get(key)
+    if request.cookies.get(key):
+        return request.cookies.get(key)
+    return DEFAULTS[key]
 
 
 if __name__ == '__main__':
