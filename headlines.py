40b6e3ecc36dc119c0213b706cd7fadc07c5cd12
johnngugi <johnngugi777@gmail.com>
2016-10-01 13:37:56 +0300

    Add weather data

--- headlines.py
+++ headlines.py
@@ -1,5 +1,9 @@
 import feedparser
 from flask import Flask, render_template, request
+import json
+from keys import WEATHER_URL
+import urllib2
+import urllib
 
 app = Flask(__name__)
 
@@ -9,18 +13,46 @@ RSS_FEEDS = {
     'nation': 'http://www.nation.co.ke/latestrss.rss'
 }
 
+DEFAULTS = {'publication': 'nation',
+            'city': 'Nairobi, KE'}
+
 
 @app.route('/')
-def get_news():
-    query = request.args.get('publication')
-    print query
+def home():
+    publication = request.args.get('publication')
+    if not publication:
+        publication = DEFAULTS['publication']
+    articles = get_news(publication)
+    city = request.args.get('city')
+    if not city:
+        city = DEFAULTS['city']
+    weather = get_weather(city)
+    return render_template('index.html', articles=articles, weather=weather)
+
+
+def get_news(query):
     if not query or query.lower() not in RSS_FEEDS:
-        publication = 'nation'
+        publication = DEFAULTS["publication"]
     else:
         publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
-    print feed
-    return render_template('index.html', articles=feed['entries'])
+    return feed['entries']
+
+
+def get_weather(query):
+    query = urllib.quote(query)
+    url = WEATHER_URL.format(query)
+    data = urllib2.urlopen(url).read()
+    parsed = json.loads(data)
+    weather = None
+    if parsed.get('weather'):
+        weather = {
+            'description': parsed['weather'][0]['description'],
+            'temperature': parsed['main']['temp'],
+            'city': parsed['name'],
+            'country': parsed['sys']['country']
+        }
+    return weather
 
 
 if __name__ == '__main__':
