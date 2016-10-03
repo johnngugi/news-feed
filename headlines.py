3241f687b53e330fd58fbe92ddf62a8958012623
johnngugi <johnngugi777@gmail.com>
2016-10-03 12:25:09 +0300

    Add ui changes

--- headlines.py
+++ headlines.py
@@ -1,6 +1,6 @@
 import datetime
 import feedparser
-from flask import Flask, render_template, request, make_response
+from flask import Flask, render_template, request, make_response,redirect
 import json
 from keys import WEATHER_URL
 import urllib2
@@ -8,23 +8,25 @@ import urllib
 
 app = Flask(__name__)
 
+# links for rss feeds
 RSS_FEEDS = {
     'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
     'cnn': 'http://rss.cnn.com/rss/edition.rss',
     'nation': 'http://www.nation.co.ke/latestrss.rss'
 }
 
+# default publication and city
 DEFAULTS = {'publication': 'nation',
             'city': 'Nairobi, KE'}
 
 
+# main page
 @app.route('/')
 def home():
     publication = get_value_with_fallback('publication')
     articles = get_news(publication)
     city = get_value_with_fallback('city')
     weather = get_weather(city)
-    print articles
     response = make_response(render_template('index.html', articles=articles, weather=weather))
     expires = datetime.datetime.now() + datetime.timedelta(days=365)
     response.set_cookie('publication', publication, expires=expires)
@@ -32,6 +34,7 @@ def home():
     return response
 
 
+# reads the xml data provided by the rss feeds and converts it to json
 def get_news(query):
     if not query or query.lower() not in RSS_FEEDS:
         publication = DEFAULTS["publication"]
@@ -41,9 +44,12 @@ def get_news(query):
     return feed['entries']
 
 
+# retreives data from weather api
 def get_weather(query):
+    # escapes special charachters in url
     query = urllib.quote(query)
     url = WEATHER_URL.format(query)
+    # opens the url provided and reads it
     data = urllib2.urlopen(url).read()
     parsed = json.loads(data)
     weather = None
@@ -57,6 +63,7 @@ def get_weather(query):
     return weather
 
 
+# provides a fallback for all requests made by user, if no request made fallbacks to DEFAULTS
 def get_value_with_fallback(key):
     if request.args.get(key):
         return request.args.get(key)
